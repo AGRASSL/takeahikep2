af9f1c26019e4447c3881be8c2159b66763617eb
andrewfriedman <andrewfriedman20@gmail.com>
2021-05-12 17:22:10 -0500

    connection with JAWSDB etc

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,18 @@ const Sequelize = require('sequelize');
 require('dotenv').config()
 
 
-module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorsAliases: false,
+module.exports = process.env.JAWSDB_URL 
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-});
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+  });
