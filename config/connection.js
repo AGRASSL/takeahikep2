914a3609057d8c0859c8874904a469a8e65bd9b1
andrewfriedman <andrewfriedman20@gmail.com>
2021-05-12 17:00:02 -0500

    update connection to req env; added process env commands line 5

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
-module.exports = new Sequelize('trails_db', 'root', 'Danzig55', {
+require('dotenv').config()
+
+
+module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   operatorsAliases: false,
