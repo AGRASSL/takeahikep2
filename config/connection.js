81372cc68ee850e9e2e1a1d311e775a05410dba5
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-05-16 22:50:13 -0500

    Fixed connection file. again

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 const Sequelize = require('sequelize');
-require('dotenv').config()
+
+require('dotenv').config();
 
 
-module.exports = process.env.JAWSDB_URL 
-? new Sequelize(process.env.JAWSDB_URL)
-: new Sequelize((process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
-    operatorsAliases: false,
-
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
+    port: 3306
   });
+}
+
+
+module.exports = sequelize;
