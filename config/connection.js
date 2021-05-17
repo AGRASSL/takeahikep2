f07210bede105d791d8bbfc43ee37b79dfa71cab
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-05-16 22:56:55 -0500

    connection reversion

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+require('dotenv').config()
 
-let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+module.exports = process.env.JAWSDB_URL 
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
-  });
-}
+    operatorsAliases: false,
 
-module.exports = sequelize;
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+  });
